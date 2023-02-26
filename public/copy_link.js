bbb3f29b5eddf6cffc0c62e53a53d5b03446ddc4
fanifrancs <fanikufran6@gmail.com>
2023-02-26 01:07:40 +0100

    update

--- public/copy_link.js
+++ public/copy_link.js
@@ -1,9 +1,20 @@
+// async function copyLink(param) {
+//   await navigator.clipboard.writeText(`Click this link to send me an anonymous message ${param} I won't know who sent it 😎`)
+//   .then(() => {
+//     alert('Link copied to clipboard.');
+//   })
+//   .catch(err => {
+//     alert(err);
+//   })
+// }
+
 async function copyLink(param) {
-  await navigator.clipboard.writeText(`Click this link to send me an anonymous message ${param} I won't know who sent it 😎.`)
-  .then(() => {
-    alert('Link copied to clipboard.');
-  })
-  .catch(err => {
-    alert(err);
-  })
+  try {
+    await navigator.clipboard.writeText(`Click this link to send me an anonymous message ${param} I won't know who sent it 😎`)
+    .then(() => {
+      alert('Link copied to clipboard.');
+    })
+  } catch (error) {
+    alert(error);
+  }
 }
